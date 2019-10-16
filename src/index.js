bde0ac75040ba9b5e29217d1e5d2072160e0be6c
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-10-17 00:10:04 +0200

    js logic for dropdown done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Tooltip from "./ui-components/tooltip";
+import Dropdown from "./ui-components/dropdown";
 
 console.warn("index.js ve started");
 
@@ -8,3 +9,9 @@ tooltip1.init();
 // second tooltip
 const tooltip2 = new Tooltip(document.querySelector("#tooltip2"));
 tooltip2.init();
+
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
