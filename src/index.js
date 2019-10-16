8ed5e31ad1317537c243a3d7955566de324bf219
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-10-16 22:52:52 +0200

    tooltip component js and css created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import "./styles.css";
+import Tooltip from "./ui-components/tooltip";
 
 console.warn("index.js ve started");
+
+// first tooltip
+const tooltip1 = new Tooltip(document.querySelector("#tooltip1"));
+tooltip1.init();
+// second tooltip
+const tooltip2 = new Tooltip(document.querySelector("#tooltip2"));
+tooltip2.init();
